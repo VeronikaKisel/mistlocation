b1fc61c2d95d559c3ef3781758bceaa0fbb69073
Markus Lehtola <markus.lehtola@aalto.fi>
2016-11-26 13:52:46 +0200

    add files

--- web-app.php
+++ web-app.php
@@ -4,13 +4,13 @@ require('phpMQTT.php');
 
 // set configuration values
 $config = array(
-  'org_id' => 'IOTF-ORG-ID',
+  'org_id' => '2lu4fi',
   'port' => '1883',
   'app_id' => 'phpmqtt',
-  'iotf_api_key' => 'IOTF-API-KEY',
-  'iotf_api_secret' => 'IOTF-API-TOKEN',
-  'maps_api_key' => 'GOOGLE-API-KEY',
-  'device_id' => 'DEVICE-ID',
+  'iotf_api_key' => 'a-2lu4fi-xp7p4e1lfv',
+  'iotf_api_secret' => 'gH?Gr&VX*@ohw6-3Me',
+  'maps_api_key' => 'AIzaSyCzwr7HRT5cQHE5SwZ6uMTv8q2tqy4m3n0',
+  'device_id' => 'oneplus1',
   'qos' => 1  
 );
 
@@ -68,7 +68,7 @@ function getLocation($topic, $msg) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Display Location</title>
+    <title>UPM Misty</title>
     <style>
     html, #content, #map {
       height: 100%;
@@ -81,6 +81,10 @@ function getLocation($topic, $msg) {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap-theme.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>    
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -88,25 +92,102 @@ function getLocation($topic, $msg) {
     <meta http-equiv="refresh" content="10">
   </head>
   <body>
-    <div id="content">
-    
-      <div class="panel panel-default">
-        <div class="panel-heading">Location of device <strong><?php echo $config['device_id']; ?></strong></div>
-        <div class="panel-body">
-          <?php if (isset($mapsApiUrl)): ?>
-          <img class="img-responsive" id="mapImage" src="<?php echo $mapsApiUrl; ?>" />  
-          <?php else: ?>
-          No GPS data available.
-          <?php endif; ?>
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">UPM Misty</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#download">Orders</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#features">Shipping</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
         </div>
-      </div>
-      
-      <div id="footer">
-        This page will automatically reload every 10 seconds. <br/>
-        <img src="powered-by-google-on-white.png" /> <br />
-        <a href="terms.html">Legal Notices</a>
-      </div>
-    </div>
-    
+        <!-- /.container-fluid -->
+    </nav>
+
+    <header>
+        <div class="container" style="margin-top: 50px;">
+            <div class="row">
+                <div class="col-sm-7">
+                    <div class="header-content">
+                        <div class="header-content-inner">
+                            
+                            <?php if (isset($mapsApiUrl)): ?>
+                            <img class="img-responsive" id="mapImage" src="<?php echo $mapsApiUrl; ?>" />  
+                            <?php else: ?>
+                            No GPS data available.
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-5">
+                    <div class="device-container">
+                        <div class="device-mockup iphone6_plus portrait white">
+                            <div class="device">
+                                <div class="screen">
+                                    <!-- Demo image for screen mockup, you can put an image here, some HTML, an animation, video, or anything else! -->
+                                    <h1>Track your deliveries on UPM Misty</h1>
+                                </div>
+                                <div class="button">
+                                    <!-- You can hook the "home button" to some JavaScript events or just remove it -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    </section>
+    <section id="contact" class="contact bg-primary">
+        <div class="container">
+            <h2>We <i class="fa fa-heart"></i> new friends!</h2>
+            <ul class="list-inline list-social">
+                <li class="social-twitter">
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                </li>
+                <li class="social-facebook">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li class="social-google-plus">
+                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <p><img src="powered-by-google-on-white.png" /> <br /></p>
+            <ul class="list-inline">
+                <li>
+                    <a href="#">Privacy</a>
+                </li>
+                <li>
+                    <a href="#">Terms</a>
+                </li>
+                <li>
+                    <a href="#">FAQ</a>
+                </li>
+            </ul>
+        </div>
+    </footer>
   </body>
 </html>
